5a1f8c378b2706c1b1c4c11acfb00650da65a399
Mazen Youssef <98108192+Mazen1004@users.noreply.github.com>
2024-05-18 19:58:29 -0400

    levels page

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -4,12 +4,27 @@ import './Dashboard.css';
 
 const Dashboard = () => {
   return (
-    <div>
-        <Link to="/codereview">
-          <button className="start-button">
-            <span className="button-text">Easy</span>
-          </button>
-        </Link>
+    <div className="dashboard-container">
+      <h1>
+        Hey <span className="highlight">Sarah!</span> Ready to step up your <span className="highlight">code review</span> game?
+      </h1>
+      <h2>Choose your level!</h2>
+      <div className="levels">
+        <div className="level beginner">
+          <span className="level-tag">Beginner</span>
+        </div>
+        <div className="level medium">
+          <span className="level-tag">Medium</span>
+        </div>
+        <div className="level hard">
+          <span className="level-tag">Hard</span>
+        </div>
+      </div>
+      <Link to="/codereview">
+        <button className="next-button">
+          <span className="button-text">Next</span>
+        </button>
+      </Link>
     </div>
   );
 };
