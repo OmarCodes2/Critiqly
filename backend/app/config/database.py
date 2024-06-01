5824007ebd104a104e69537d000def824b61aaf8
DeZhaysun <shuaihangtang@gmail.com>
2024-06-01 15:29:08 -0400

    backend file support

--- backend/app/config/database.py
+++ backend/app/config/database.py
@@ -19,94 +19,99 @@ users=db["users"]
 #Function for inserting easy level
 def create_easy_level():
     easy_level = Level(
-        difficulty="easy",
+        repo="easy_example",
         number_of_mistakes = 2,
         mistakes_found = 0,
-        lines=[
-            CodeLine(
-                line_number=1,
-                is_modified = False,
-                versions=[
-                    CodeVersion(id=1, code="def example_function():"),
-                ]
-            ),
-            CodeLine(
-                line_number=2,
-                is_modified = False,
-                versions=[
-                    CodeVersion(id=1, code="    print('Starting function')"),
-                ]
-            ),
-            CodeLine(
-                line_number=3,
-                is_modified = False,
-                versions=[
-                    CodeVersion(id=1, code="    x = 10"),
-                ]
-            ),
-            CodeLine(
-                line_number=4,
-                is_modified = False,
-                versions=[
-                    CodeVersion(id=1, code="    y = 20"),
-                ]
-            ),
-            CodeLine(
-                line_number=5,
-                is_modified = True,
-                is_correct = False,
-                versions=[
-                    CodeVersion(id=1, code="    result = f + y"),
-                    CodeVersion(id=2, code="    result = f x y"),
-                    CodeVersion(id=3, code="    result = f * y"),
-                ]
-            ),
-            CodeLine(
-                line_number=6,
-                is_modified = True,
-                is_correct = True,
-                versions=[
-                    CodeVersion(id=1, code="    print(f'The sum of x and y is: {result}')"),
-                    CodeVersion(id=3, code="    print(f'The product of x and y is: {result}')"),  # Corrected
-                ]
-            ),
-            CodeLine(
-                line_number=7,
-                is_modified = True,
-                is_correct = False,
-                versions=[
-                    CodeVersion(id=1, code="    if result > 20:"),
-                    CodeVersion(id=2, code="    if result > 20"),  
-                    CodeVersion(id=3, code="    if result > 20:"),  
-                ]
-            ),
-            CodeLine(
-                line_number=8,
-                is_modified = False,
-                versions=[
-                    CodeVersion(id=1, code="        print('Result is greater than 20')"),
-                ]
-            ),
-            CodeLine(
-                line_number=9,
-                is_modified = False,
-                versions=[
-                    CodeVersion(id=1, code="    else:"),
-                ]
-            ),
-            CodeLine(
-                line_number=10,
-                is_modified = False,
-                versions=[
-                    CodeVersion(id=1, code="        print('Result is not greater than 20')"),
-                ]
-            ),
-            CodeLine(
-                line_number=11,
-                is_modified = False,
-                versions=[
-                    CodeVersion(id=1, code="    return result"),
-                ]
+        files = [
+            File(
+                filename = "main.py",
+                lines=[
+                    CodeLine(
+                        line_number=1,
+                        is_modified = False,
+                        versions=[
+                            CodeVersion(id=1, code="def example_function():"),
+                        ]
+                    ),
+                    CodeLine(
+                        line_number=2,
+                        is_modified = False,
+                        versions=[
+                            CodeVersion(id=1, code="    print('Starting function')"),
+                        ]
+                    ),
+                    CodeLine(
+                        line_number=3,
+                        is_modified = False,
+                        versions=[
+                            CodeVersion(id=1, code="    x = 10"),
+                        ]
+                    ),
+                    CodeLine(
+                        line_number=4,
+                        is_modified = False,
+                        versions=[
+                            CodeVersion(id=1, code="    y = 20"),
+                        ]
+                    ),
+                    CodeLine(
+                        line_number=5,
+                        is_modified = True,
+                        is_correct = False,
+                        versions=[
+                            CodeVersion(id=1, code="    result = f + y"),
+                            CodeVersion(id=2, code="    result = f x y"),
+                            CodeVersion(id=3, code="    result = f * y"),
+                        ]
+                    ),
+                    CodeLine(
+                        line_number=6,
+                        is_modified = True,
+                        is_correct = True,
+                        versions=[
+                            CodeVersion(id=1, code="    print(f'The sum of x and y is: {result}')"),
+                            CodeVersion(id=3, code="    print(f'The product of x and y is: {result}')"),  # Corrected
+                        ]
+                    ),
+                    CodeLine(
+                        line_number=7,
+                        is_modified = True,
+                        is_correct = False,
+                        versions=[
+                            CodeVersion(id=1, code="    if result > 20:"),
+                            CodeVersion(id=2, code="    if result > 20"),  
+                            CodeVersion(id=3, code="    if result > 20:"),  
+                        ]
+                    ),
+                    CodeLine(
+                        line_number=8,
+                        is_modified = False,
+                        versions=[
+                            CodeVersion(id=1, code="        print('Result is greater than 20')"),
+                        ]
+                    ),
+                    CodeLine(
+                        line_number=9,
+                        is_modified = False,
+                        versions=[
+                            CodeVersion(id=1, code="    else:"),
+                        ]
+                    ),
+                    CodeLine(
+                        line_number=10,
+                        is_modified = False,
+                        versions=[
+                            CodeVersion(id=1, code="        print('Result is not greater than 20')"),
+                        ]
+                    ),
+                    CodeLine(
+                        line_number=11,
+                        is_modified = False,
+                        versions=[
+                            CodeVersion(id=1, code="    return result"),
+                        ]
+                    ),
+                ],
             ),
         ],
         readme="This is an easy level with a function that calculates the sum of two numbers. The goal of your coworker was to change it to multiplication of the two numbers. There are two mistakes with the new additions that need to be fixed."
