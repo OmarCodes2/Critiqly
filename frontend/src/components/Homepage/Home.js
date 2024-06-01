d0228453650af6fbccda254c98eb480a47f96795
Mazen Youssef <98108192+Mazen1004@users.noreply.github.com>
2024-06-01 12:07:17 -0400

    setup homepage using tailwind

--- frontend/src/components/Homepage/Home.js
+++ frontend/src/components/Homepage/Home.js
@@ -1,30 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Home.css';
 import logoPath from '../../assets/images/Homepage/logo.png';
 import imagePath from '../../assets/images/Homepage/buttom_image.png';
 
 const Home = () => {
   return (
-    <div className="home-container">
-      <header className="header">
-        <div className="left-container">
-        <img src={logoPath} alt="Logo" className="logo-image" />
-        <div className="logo">Critqly</div>
+    <div className="flex flex-col box-border">
+      <header className="w-full h-[60px] flex items-center bg-[#1a1a1a] p-5 box-border fixed top-0 left-0">
+        <div className="flex items-center">
+        <img src={logoPath} alt="Logo" className="h-[1.5em] mr-2" />
+        <div className="text-2xl font-bold">Critqly</div>
         </div>
       </header>
-      <main className="main-content">
-        <h1 className="title">Review <span className="highlight">Better</span><br />Code <span className="highlight">Smarter</span></h1>
-        <p className="text-[18px] text-black text-opacity-80">
+      <main className="flex flex-col items-center gap-5 pt-[15em]">
+        <h1 className="text-4xl font-bold m-0">Review <span className="highlight1">Better</span><br />Code <span className="highlight1">Smarter</span></h1>
+        <p className="text-[18px] text-white text-opacity-80">
           Take your code review to the next level with the help of AI-powered feedback
         </p>
         <Link to="/signup">
-          <button className="start-button">
-            <img src={logoPath} alt="Logo" className="logo-image" />
-            <span className="button-text">Start your journey</span>
+          <button className="inline-flex items-center gap-3.5 p-4 rounded-full border border-white border-opacity-10 bg-white bg-opacity-10 hover:bg-opacity-20 cursor-pointer">
+            <img src={logoPath} alt="Logo" className="h-[1.5em] mr-2" />
+            <span className="text-base text-white text-opacity-80">Start your journey</span>
           </button>
         </Link>
-        <img src={imagePath} alt="" className="bottom-image" />
+        <img src={imagePath} alt="buttom"/>
       </main>
     </div>
   );
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden"
+          },
+          "100%": {
+            width: "100%"
+          }
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent"
+          },
+          "100%": {
+            borderColor: "white"
+          }
+        }
+      },
+      animation: {
+        typing: "typing 2s steps(20) infinite alternate, blink .7s infinite"
+      }
+    },
   },
   plugins: [],
 }
